8b713a6dfe44ac11870d4a512c775813fdcdc884
Getinet Alewab <getinetbr@yahoo.com>
2021-09-13 22:45:00 -0400

    Code New User Function, closes #7

--- shopme-web/shopme-backend/src/main/java/com/jabirinc/shopmebackend/controller/UserController.java
+++ shopme-web/shopme-backend/src/main/java/com/jabirinc/shopmebackend/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.jabirinc.shopmebackend.controller;
 
 import com.jabirinc.shopmebackend.user.UserService;
+import com.jabirinc.shopmecommon.entity.Role;
 import com.jabirinc.shopmecommon.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -27,5 +30,25 @@ public class UserController {
         List<User> listOfUsers = userService.listAll();
         model.addAttribute("listOfUsers",listOfUsers);
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String newUser(Model model) {
+
+        List<Role> listRoles = userService.loadRoles();
+
+        model.addAttribute("user", new User());
+        model.addAttribute("listRoles", listRoles);
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes) {
+
+        //System.out.println(user);
+        userService.save(user);
+        redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+
+        return "redirect:/users";
     }
 }
--- shopme-web/shopme-backend/src/main/java/com/jabirinc/shopmebackend/user/UserService.java
+++ shopme-web/shopme-backend/src/main/java/com/jabirinc/shopmebackend/user/UserService.java
@@ -1,5 +1,6 @@
 package com.jabirinc.shopmebackend.user;
 
+import com.jabirinc.shopmecommon.entity.Role;
 import com.jabirinc.shopmecommon.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,13 +14,23 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     public List<User> listAll() {
         return (List<User>) userRepository.findAll();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<Role> loadRoles() {
+        return (List<Role>) roleRepository.findAll();
     }
 }
