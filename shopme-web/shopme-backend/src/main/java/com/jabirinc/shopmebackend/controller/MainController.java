55019e8e9d9d0e590bf38df201d43b19150aae98
Getinet Alewab <getinetbr@yahoo.com>
2021-09-28 12:37:56 -0400

    48 - Code User Login Page, closes #27

--- shopme-web/shopme-backend/src/main/java/com/jabirinc/shopmebackend/controller/MainController.java
+++ shopme-web/shopme-backend/src/main/java/com/jabirinc/shopmebackend/controller/MainController.java
@@ -13,4 +13,9 @@ public class MainController {
     public String viewHomePage() {
         return "index";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
