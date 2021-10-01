d79cf3f3d71e89274af070412f7fd6da41fa922c
Getinet Alewab <getinetbr@yahoo.com>
2021-10-01 10:45:33 -0400

    58 - Code User Authorization Basics, closes #37

--- shopme-web/shopme-backend/src/main/java/com/jabirinc/shopmebackend/config/ShopmeUserDetailsService.java
+++ shopme-web/shopme-backend/src/main/java/com/jabirinc/shopmebackend/config/ShopmeUserDetailsService.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Getinet on 9/28/21
  */
+@Service
 public class ShopmeUserDetailsService implements UserDetailsService {
 
     @Autowired
