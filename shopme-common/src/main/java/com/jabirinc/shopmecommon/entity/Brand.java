0fc62a8140dcbc9454990835b3c4b765bb076df0
Getinet Alewab <getinetbr@yahoo.com>
2021-10-11 16:37:32 -0400

    85 - Code Brand Entity and Repository, closes #65

---
+++ shopme-common/src/main/java/com/jabirinc/shopmecommon/entity/Brand.java
@@ -0,0 +1,39 @@
+package com.jabirinc.shopmecommon.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Getinet on 10/11/21
+ */
+@Setter
+@Getter
+@NoArgsConstructor
+
+@Entity()
+@Table(name = "brands")
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 45, nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 128, nullable = false)
+    private String logo;
+
+    @ManyToMany
+    @JoinTable(
+            name = "brands_categories",
+            joinColumns = @JoinColumn(name = "brand_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
+}
---
+++ shopme-web/shopme-backend/src/main/java/com/jabirinc/shopmebackend/brand/BrandRepository.java
@@ -0,0 +1,11 @@
+package com.jabirinc.shopmebackend.brand;
+
+import com.jabirinc.shopmecommon.entity.Brand;
+import com.jabirinc.shopmecommon.entity.Category;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * Created by Getinet on 10/11/21
+ */
+public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer> {
+}
