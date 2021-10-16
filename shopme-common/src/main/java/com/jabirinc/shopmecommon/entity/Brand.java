8c85e33902c889e55662feef7d3d0769ea9d08fd
Getinet Alewab <getinetbr@yahoo.com>
2021-10-15 22:14:17 -0400

    88 - Code Brand Form, closes #67

--- shopme-common/src/main/java/com/jabirinc/shopmecommon/entity/Brand.java
+++ shopme-common/src/main/java/com/jabirinc/shopmecommon/entity/Brand.java
@@ -21,6 +21,10 @@ import java.util.Set;
 @Table(name = "brands")
 public class Brand {
 
+    public static final String DEFAULT_BRAND_IMAGE_NAME = "image-thumbnail.png";
+    public static final String DEFAULT_BRAND_IMAGE_PATH = "/images/" + DEFAULT_BRAND_IMAGE_NAME;
+    public static final String BRAND_IMAGES_DIR = "/brand-images/";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -38,5 +42,15 @@ public class Brand {
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
     private Set<Category> categories = new HashSet<>();
+
+
+    @Transient
+    public String getBrandImagePath() {
+
+        if (id == null) {
+            return DEFAULT_BRAND_IMAGE_PATH;
+        }
+        return BRAND_IMAGES_DIR + this.id + "/" + this.logo;
+    }
 
 }
--- shopme-web/shopme-backend/src/main/java/com/jabirinc/shopmebackend/user/controller/BrandController.java
+++ shopme-web/shopme-backend/src/main/java/com/jabirinc/shopmebackend/user/controller/BrandController.java
@@ -1,7 +1,9 @@
 package com.jabirinc.shopmebackend.user.controller;
 
 import com.jabirinc.shopmebackend.brand.BrandService;
+import com.jabirinc.shopmebackend.category.CategoryService;
 import com.jabirinc.shopmecommon.entity.Brand;
+import com.jabirinc.shopmecommon.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,11 +21,14 @@ public class BrandController {
 
     private static final String BRANDS_ROOT_REQ_PATH = "/brands";
     private static final String BRANDS_REQ_PATH = "brands/brands";
+    private static final String BRAND_FORM_REQ_PATH = "brands/brand_form";
     private final BrandService brandService;
+    private final CategoryService categoryService;
 
     @Autowired
-    public BrandController(BrandService brandService) {
+    public BrandController(BrandService brandService, CategoryService categoryService) {
         this.brandService = brandService;
+        this.categoryService = categoryService;
     }
 
     @GetMapping()
@@ -31,5 +36,15 @@ public class BrandController {
         List<Brand> listBrands = brandService.listAll();
         model.addAttribute("listBrands", listBrands);
         return BRANDS_REQ_PATH;
+    }
+
+    @GetMapping("/new")
+    public String newBrand(Model model) {
+
+        List<Category> listCategories = categoryService.listAllCategoriesUsedInForm();
+        model.addAttribute("brand", new Brand());
+        model.addAttribute("listCategories", listCategories);
+        model.addAttribute("pageTitle", "Create New Brand");
+        return BRAND_FORM_REQ_PATH;
     }
 }
